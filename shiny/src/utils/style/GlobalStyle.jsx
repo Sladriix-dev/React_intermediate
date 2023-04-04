89290f3cf95ee3538f173af4cac90198623041d1
Sladriix-dev <sladriix@gmail.com>
2023-04-04 13:39:42 +0200

    Aller plus loin avec les hooks - Refonte du darkmode + créa de hook pour le theme

--- shiny/src/utils/style/GlobalStyle.jsx
+++ shiny/src/utils/style/GlobalStyle.jsx
@@ -1,23 +1,21 @@
-import { useContext } from "react"
-import { createGlobalStyle } from "styled-components"
-import { ThemeContext } from "../context"
+import { createGlobalStyle } from 'styled-components'
+import { useTheme } from '../hooks'
 
 const StyledGlobalStyle = createGlobalStyle`
-  * {
-    font-family: "Trebuchet MS", Helvetica, sans-serif;
-    background-color: ${({ isDarkMode }) => (isDarkMode ? 'black' : 'white')} !important;
-    color: ${({ isDarkMode }) => (isDarkMode ? 'white' : 'black')} !important;
-  }
-
-  body {
-    margin: 0;
-  }
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+        background-color: ${(props) =>
+          props.isDarkMode ? '#2F2E41' : 'white'};
+        margin: 0;
+    }
 `
 
-export default function GlobalStyle() {
-    const { theme } = useContext(ThemeContext)
+function GlobalStyle() {
+  const { theme } = useTheme()
 
-    return (
-        <StyledGlobalStyle isDarkMode={theme === 'dark'}/>
-    )
+  return <StyledGlobalStyle isDarkMode={theme === 'dark'} />
 }
+
+export default GlobalStyle
